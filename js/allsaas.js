54ba1bdd0e61944bc144c9b1155dddad3cefa23e
Jim <616692289@qq.com>
2019-09-16 23:41:24 +0800

    修改更新内容读取方式

--- js/allsaas.js
+++ js/allsaas.js
@@ -1,28 +1,6 @@
-      function test(){
-        var ac;
 
-      $.ajax({
-        "async": false,
-        "crossDomain": true,
-        "url": "https://www.yuque.com/api/v2/repos/keeplearning/cv3026/docs/tp6ili",
-        "method": "GET",
-        "headers": {
-          "X-Auth-Token": "rvf7keaUUCdPDlQxcl5iVUPckWYssVGKYQeYMHka",
-          "Content-Type": "application/json",
-        },
-         "data": {
-           "raw":1
-         },
-             success: function (json) {
-                 ac = json;
-                 console.log(ac);
-             }
-         });
-        //  console.log(ac);
-         return ac.data.body_draft;
-}
 
-var src = test();
+var src = yuquejson.data.body;
 var content = {
     "nav": [],
     "header": {},
